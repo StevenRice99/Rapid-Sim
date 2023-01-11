9b0dbe338ad9871ae9f8266ebe6fad84cb3cf8e1
Steven Rice <TheStevenRice@Gmail.com>
2023-01-11 11:58:52 -0500

    Best dataset saving yet

--- Assets/Scripts/RapidSim/Networks/Dataset.cs
+++ Assets/Scripts/RapidSim/Networks/Dataset.cs
@@ -10,32 +10,21 @@ namespace RapidSim.Networks
     [CreateAssetMenu(fileName = "Dataset", menuName = "Dataset", order = 1)]
     public class Dataset : ScriptableObject
     {
-        [HideInInspector]
-        [Tooltip("The number of data points.")]
-        [SerializeField]
-        private int size;
-        
         [Tooltip("The data points of the dataset.")]
         [SerializeField]
         private List<DataPoint> dataPoints;
 
         public List<DataPoint> DataPoints => dataPoints;
 
-        public int Size => size;
+        public int Size => dataPoints.Count;
 
         public void Add(double[] inputs, double[] outputs)
         {
             dataPoints.Add(new(inputs, outputs));
-            OnValidate();
 #if UNITY_EDITOR
             EditorUtility.SetDirty(this);
 #endif
-            Debug.Log($" Dataset {name} | {size} Points");
-        }
-
-        private void OnValidate()
-        {
-            size = dataPoints.Count;
+            Debug.Log($" Dataset {name} | {Size} Points");
         }
     }
 }
