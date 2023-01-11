31dfa7812ba29fadba4f39cca1aed4b7c2eaadc1
Steven Rice <TheStevenRice@Gmail.com>
2023-01-11 11:57:40 -0500

    Dataset updates

--- Assets/Scripts/RapidSim/Networks/Dataset.cs
+++ Assets/Scripts/RapidSim/Networks/Dataset.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace RapidSim.Networks
 {
     [CreateAssetMenu(fileName = "Dataset", menuName = "Dataset", order = 1)]
@@ -23,6 +27,9 @@ namespace RapidSim.Networks
         {
             dataPoints.Add(new(inputs, outputs));
             OnValidate();
+#if UNITY_EDITOR
+            EditorUtility.SetDirty(this);
+#endif
             Debug.Log($" Dataset {name} | {size} Points");
         }
 
